b86c01848f54116875926e1a6f39588ec7064865
Yidah <yidahit@hotmail.com>
2020-12-18 14:32:11 +0000

    Added my middleware and action creators, install redux-thunk

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,9 @@ import {connect} from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+// import * as actionTypes from '../../store/actions/actions';
+import * as actionCreators from '../../store/actions/actions';
+
 
 class Counter extends Component {
 
@@ -62,12 +64,12 @@ const mapStateToProps = state => {
 // dispatch helper function is provided by redux 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: ()=> dispatch({type: actionTypes.INCREMENT}),
-        onDecrementCounter: ()=> dispatch({type: actionTypes.DECREMENT}),
-        onAddCounter: ()=> dispatch({type: actionTypes.ADD, value:5}),
-        onSubtractCounter: ()=> dispatch({type: actionTypes.SUBTRACT, value:5}),
-        onStoreResult:(result)=> dispatch({type:actionTypes.STORE_RESULT, res:result}),
-        onDeleteResult:(id)=> dispatch({type:actionTypes.DELETE_RESULT, resultElId:id})
+        onIncrementCounter: ()=> dispatch(actionCreators.increment()),
+        onDecrementCounter: ()=> dispatch(actionCreators.decrement()),
+        onAddCounter: ()=> dispatch(actionCreators.add(5)),
+        onSubtractCounter: ()=> dispatch(actionCreators.subtract(5)),
+        onStoreResult:(result)=> dispatch(actionCreators.storeResult(result)),
+        onDeleteResult:(id)=> dispatch(actionCreators.deleteResult(id))
 
     };
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
 
 import counterReducer from './store/reducers/counter';
@@ -15,7 +15,24 @@ const rootReducer = combineReducers({
 
 });
 
-const store = createStore(rootReducer);
+// store, next and action are functions executed by Redux
+const logger = store =>{
+    return next =>{
+        return action => {
+            // Here we add the code we want to execute between the action an the reducer
+            console.log('Middleware dispatching action:', action);
+            // thhis will let the action continue to the reducer, 
+            // to succeed, we need to pass the action
+            const result =next(action);
+            console.log('Middleware next state', store.getState())
+            return result;
+
+        }
+    }
+}
+
+// apply middleware (or so called enhancer) by using and importing applyMiddleware
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
